013105b7252a4ba87f9dc94533f0a0b21b74975d
worldsnas <worldsnas@gmail.com>
2017-10-16 09:46:07 +0330

    adding all project to repo

--- src/main/java/com/worldsnas/volleyhelper/ResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.worldsnas.volleyhelper;
-
-/**
- * Created by jafari on 9/10/2017.
- */
-
-public interface ResponseListener<T> {
-    void onResponse(T response, int responseCode, long responseTime);
-}
